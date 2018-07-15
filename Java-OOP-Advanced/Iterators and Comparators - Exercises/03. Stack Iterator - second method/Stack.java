8be436689a4c1e9b49a23f39b2b06aeeb999cbe3
XuTkO <xumko@abv.bg>
2018-07-15 17:07:04 +0300

    some changes to 3

---
+++ Java-OOP-Advanced/Iterators and Comparators - Exercises/03. Stack Iterator - second method/Stack.java
@@ -0,0 +1,59 @@
+package stackIterator;
+
+import java.util.*;
+
+public class Stack<T> implements Iterable<T>{
+    private List<T> elements;
+    private int elementCount ;
+
+    public Stack() {
+        this.elements = new ArrayList<>();
+    }
+
+    public void push(T element){
+        this.elements.add(elementCount++, element);
+
+    }
+
+    public T pop(){
+        if (this.elementCount ==0){
+            throw new NoSuchElementException();
+        }
+        T element =  this.elements.get(--elementCount);
+        this.elements.remove(elementCount);
+        return element;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new StackIterator();
+    }
+    private  final class StackIterator implements Iterator<T>{
+        private int cursor;
+
+        public StackIterator() {
+            this.cursor = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return elements.size() > this.cursor;
+        }
+
+        @Override
+        public T next() {
+            return elements.get(cursor++);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int a = elements.size() - 1; a >= 0; a--){
+            sb.append(elements.get(a)).append(System.lineSeparator());
+        }
+
+        return sb.toString();
+    }
+}
